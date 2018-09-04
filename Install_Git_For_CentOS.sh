c277282a1726f1bde37be637ada8dcc733ab7c4a
cutefrank <littlescore@gmail.com>
2018-09-04 09:35:09 +0800

    -y

--- Install_Git_For_CentOS.sh
+++ Install_Git_For_CentOS.sh
@@ -3,13 +3,13 @@
 # CentOS 7 install git
 
 # delete old git
-sudo yum remove git
+sudo yum remove git -y
 
 # Install epel-release repository
-sudo yum install epel-release
+sudo yum install epel-release -y
 
 # Install IUS repository (For RHEL/CentOS 7)
-sudo yum install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum install https://centos7.iuscommunity.org/ius-release.rpm -y
 
 # Install git2u
-sudo yum install git2u
+sudo yum install git2u -y
